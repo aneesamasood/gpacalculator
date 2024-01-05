1c1ebc9790555c61d1b11cee10ca384064d2c465
aneesamasood <146127022+aneesamasood@users.noreply.github.com>
2024-01-05 16:57:37 -0500

    Delete gpacalculator directory

--- gpacalculator/gpaapp/models.py
+++
@@ -1,7 +0,0 @@
-# models.py
-from django.db import models
-
-class Course(models.Model):
-    percentage = models.IntegerField()
-    parent_course = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
-
